9cd0be90cd6b9d148f05b11704aca6f45940a5c3
Eshan Chatterjee <eshan2001221@gmail.com>
2023-09-18 15:24:47 +0530

    added User constructor

--- src/user.rs
+++ src/user.rs
@@ -6,3 +6,15 @@ pub struct User {
     name: String,
     home_dir: String, // probably change to Path later
 }
+
+impl User {
+    pub fn new(user_name: String, name: String) -> User {
+        let random_bytes = rand::random();
+        User {
+            uid: uuid::Builder::from_random_bytes(random_bytes).into_uuid(),
+            user_name,
+            name,
+            home_dir: String::new()
+        }
+    }
+}
